273326b09b944a2d0b6d970ade0dbab2549284fc
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2023-05-09 01:06:25 +0200

    modified file

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0]
 const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-
+const navItems = document.querySelectorAll('.nav-item');
 toggleBtn.addEventListener("click",()=>{
     navbarLinks.classList.toggle("active")
 })
 
+const currentPath = window.location.pathname;
+const currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+navItems.forEach(item => {
+  const link = item.querySelector('a');
+  if (link && link.getAttribute('href').endsWith(currentPage)) {
+    item.classList.add('current');
+  }
+})
 
