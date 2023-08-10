180804a15849d67b4a9dfd77dca77ff7852adf7d
Silindile Madlala <87993886+Patricia244@users.noreply.github.com>
2023-08-10 12:04:33 +0200

    modified file

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const toggleBtn = document.getElementsByClassName("toggle-btn")[0]
-const navbarLinks = document.getElementsByClassName("navbar-links")[0]
-const navItems = document.querySelectorAll('.nav-item');
-toggleBtn.addEventListener("click",()=>{
-    navbarLinks.classList.toggle("active")
-})
+const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navItems = document.querySelectorAll(".nav-item");
+toggleBtn.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
 
 const currentPath = window.location.pathname;
-const currentPage = currentPath.substring(currentPath.lastIndexOf('/') + 1);
-navItems.forEach(item => {
-  const link = item.querySelector('a');
-  if (link && link.getAttribute('href').endsWith(currentPage)) {
-    item.classList.add('current');
+const currentPage = currentPath.substring(currentPath.lastIndexOf("/") + 1);
+navItems.forEach((item) => {
+  const link = item.querySelector("a");
+  if (link && link.getAttribute("href").endsWith(currentPage)) {
+    item.classList.add("current");
   }
-})
+});
 
+function toggleCard(cardId) {
+  const card = document.getElementById(cardId);
+
+  card.classList.toggle("flipped");
+}
